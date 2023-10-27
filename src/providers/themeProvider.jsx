f5672e7d29dbec43010c85c749dc4c62ce9e2076
Pablo <102944968+TasukeKyK@users.noreply.github.com>
2023-10-27 13:12:44 +0200

    toqueteando cositas

--- src/providers/themeProvider.jsx
+++ src/providers/themeProvider.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const themes = {
   //exporta los dos temas que hay
   light: {
-    color: "#555555",
+    color: "black",
     background: "#eeeeee",
   },
   dark: {
