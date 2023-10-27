7e9fa1256b737e67f3fe494f77b02d749d324d20
Pablo <102944968+TasukeKyK@users.noreply.github.com>
2023-10-27 12:40:42 +0200

    añdir keys a map

--- src/components/tabla.jsx
+++ src/components/tabla.jsx
@@ -4,11 +4,11 @@ const Tabla = ({ users }) => {  // recibe los usuarios que va a mostrar
   const renderList = () => {  // renderiza los usuarios 
     return users.map((user) => {  // accede a cada uno de ellos y a sus propiedades
       return (
-        <tr>
-          <th>{user.name.first}</th>
-          <th>{user.name.last}</th>
-          <th>{user.email}</th>
-          <th>{user.dob.age}</th>
+        <tr key={user.name.first+user.name.last+user.email+user.dob.age}>
+          <th key={user.name.first+user.name.last}>{user.name.first}</th>
+          <th key={user.name.first+user.name.last+user.name.last}>{user.name.last}</th>
+          <th key={user.name.first+user.name.last+user.email}>{user.email}</th>
+          <th key={user.name.first+user.name.last+user.dob.age}>{user.dob.age}</th>
         </tr>
       );
     });
